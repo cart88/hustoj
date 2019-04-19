5d4087acf6d20b3d5308ebfa9a67d3cb4a4a5f07
罗培中 <lpz2@163.com>
2019-04-20 00:14:55 +0800

    solved:::映射windows目录

--- trunk/docker/hustoj/build-dokcer.sh
+++ trunk/docker/hustoj/build-dokcer.sh
@@ -2,9 +2,12 @@
 #docker rm -f -v /hustoj-dev
 #docker build -f Dockerfile -t hustoj-cpp  ./
 
-docker rm -f -v /hustoj-cpp 
+# docker rm -f -v /hustoj-cpp 
 #docker run -d -it --privileged --name hustoj-cpp -p 8080:80 hustoj-cpp
-docker rm -f /hustoj-cpp 
-docker run -d -it --privileged --name hustoj-cpp -p 8080:80 -v /home/hustoj-cpp/:/data/ hustoj-cpp
+# docker rm -f /hustoj-cpp 
+# docker run -d -it --privileged --name hustoj-cpp -p 8080:80 -v /host_mnt/d/test/hhh/mysql/:/data/ hustoj-cpp
 #docker exec -i -t hustoj-cpp /bin/bash
-
+docker stop hustoj-cpp 
+docker rm hustoj-cpp
+docker build -f Dockerfile -t hustoj-cpp  ./
+docker run -d -it --privileged --name hustoj-cpp -p 8080:80 -v /host_mnt/d/test/hhh/mysql/:/data/ hustoj-cpp
