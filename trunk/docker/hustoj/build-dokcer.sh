20fe5faf8e138b26eb379704339dda97e569422c
罗培中 <lpz2@163.com>
2019-04-20 11:09:29 +0800

    CHANGED::添加删除images

--- trunk/docker/hustoj/build-dokcer.sh
+++ trunk/docker/hustoj/build-dokcer.sh
@@ -9,5 +9,6 @@
 #docker exec -i -t hustoj-cpp /bin/bash
 docker stop hustoj-cpp 
 docker rm hustoj-cpp
+docker rmi hustoj-cpp
 docker build -f Dockerfile -t hustoj-cpp  ./
 # docker run -d -it --privileged --name hustoj-cpp -p 8080:80 -v /host_mnt/d/test/hhh/mysql/:/data/ -v /host_mnt/d/test/hhh/code/:/hustoj/trunk/ hustoj-cpp
